339f581549400596e380cca503632476f70e4c4b
jripolldev <juan.ripoll.93@gmail.com>
2022-09-22 19:39:15 +0200

    Publica ejemplo de implementación de la interfaz IComparable y uso de la sobrecarga de operadores en VB.NET.

---
+++ Box.vb
@@ -0,0 +1,106 @@
+﻿Public Class Box
+    Implements IComparable(Of Box)
+
+    'Atributos
+    Private _Alto As Single
+    Private _Ancho As Single
+    Private _Largo As Single
+
+    'Constructores
+    Public Sub New()
+        Me._Alto = 1
+        Me._Ancho = 1
+        Me._Largo = 1
+    End Sub
+
+    Public Sub New(ByVal alto As Single, ByVal ancho As Single, ByVal largo As Single)
+        If alto <= 0 OrElse ancho <= 0 OrElse largo <= 0 Then
+            Throw New ArgumentException
+        End If
+        Me._Alto = alto
+        Me._Ancho = ancho
+        Me._Largo = largo
+    End Sub
+
+    'Propiedades
+    Public Property Alto() As Single
+        Get
+            Return Me._Alto
+        End Get
+        Set(value As Single)
+            Me._Alto = value
+        End Set
+    End Property
+
+    Public Property Ancho() As Single
+        Get
+            Return Me._Ancho
+        End Get
+        Set(value As Single)
+            Me._Ancho = value
+        End Set
+    End Property
+
+    Public Property Largo() As Single
+        Get
+            Return Me._Largo
+        End Get
+        Set(value As Single)
+            Me._Largo = value
+        End Set
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return "Caja (Alto x Ancho x Largo): " & Me._Alto & "x" & Me._Ancho & "x" & Me._Largo
+    End Function
+
+    'Sobrecarga de operadores
+    Public Shared Operator +(ByVal c1 As Box, ByVal c2 As Box) As Box
+        Return New Box(c1._Alto + c2._Alto, c1._Ancho + c2._Ancho, c1._Largo + c2._Largo)
+    End Operator
+
+    Public Shared Operator -(ByVal c1 As Box, ByVal c2 As Box) As Box
+        'Falta validar que no sean 0 o negativos
+        Return New Box(c1._Alto - c2._Alto, c1._Ancho - c2._Ancho, c1._Largo - c2._Largo)
+    End Operator
+
+    'Ordenación de objetos para colecciones genéricas
+
+    Public Function CompareTo(ByVal y As Box) As Integer Implements IComparable(Of Box).CompareTo
+        If Me._Alto.CompareTo(y._Alto) <> 0 Then
+            Return Me._Alto.CompareTo(y._Alto)
+        ElseIf Me._Largo.CompareTo(y._Largo) <> 0 Then
+            Return Me._Largo.CompareTo(y._Largo)
+        ElseIf Me.Ancho.CompareTo(y._Ancho) <> 0 Then
+            Return Me.Ancho.CompareTo(y._Ancho)
+        Else
+            Return 0
+        End If
+    End Function
+
+
+    'Public Function Compare(ByVal x As Caja, ByVal y As Caja) As Integer Implements IComparer(Of Caja).Compare
+    '    If x._Alto.CompareTo(y._Alto) <> 0 Then
+    '        Return x._Alto.CompareTo(y._Alto)
+    '    ElseIf x._Largo.CompareTo(y._Largo) <> 0 Then
+    '        Return x._Largo.CompareTo(y._Largo)
+    '    ElseIf x.Ancho.CompareTo(y._Ancho) <> 0 Then
+    '        Return x.Ancho.CompareTo(y._Ancho)
+    '    Else
+    '        Return 0
+    '    End If
+    'End Function
+
+    'Destructores
+    Public Sub Dispose()
+        Me._Alto = 0
+        Me._Ancho = 0
+        Me._Largo = 0
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        Me._Alto = 0
+        Me._Ancho = 0
+        Me._Largo = 0
+    End Sub
+End Class
---
+++ Factory.vb
@@ -0,0 +1,71 @@
+﻿Module Factory
+
+    Sub Main()
+        Dim c1 As New Box(10, 10, 10),
+            c2 As New Box(20, 20, 20),
+            c3 As New Box(1, 1, 1)
+
+        Dim lista As New List(Of Box)
+        Dim listaOrdenada As New SortedList(Of Box, Box)
+
+        Console.WriteLine(c1.ToString)
+        Console.WriteLine(c2.ToString)
+        Console.WriteLine(c3.ToString)
+        Console.WriteLine()
+
+        lista.Add(c1)
+        lista.Add(c2)
+
+        listaOrdenada.Add(c1, c1)
+        listaOrdenada.Add(c2, c2)
+
+        'Probamos a sumar
+        c3 = c1 + c2
+        Console.WriteLine(c3.ToString)
+        Console.WriteLine()
+
+        lista.Add(c3)
+        listaOrdenada.Add(c3, c3)
+
+        'Probamos a restar
+        c3 = c2 - c1
+        Console.WriteLine(c3.ToString)
+        Console.WriteLine()
+        lista.Add(c3)
+
+        Try
+            listaOrdenada.Add(c3, c3)
+        Catch ex As ArgumentException
+            Console.WriteLine("El elemento " & c3.ToString & " ya se encuentra en la lista.")
+            Console.WriteLine()
+        End Try
+
+        Console.WriteLine("Lista antes de ordenar--------------------------")
+        For Each elemento In lista
+            Console.WriteLine(elemento.ToString)
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine("Lista después de ordenar--------------------------")
+        lista.Sort()
+        For Each elemento In lista
+            Console.WriteLine(elemento.ToString)
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine("Lista ordenada --------------------------")
+        For Each elemento In listaOrdenada
+            Console.WriteLine(elemento.ToString)
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine("Lista ordenada inversa--------------------------")
+        For Each elemento In listaOrdenada.Reverse()
+            Console.WriteLine(elemento.ToString)
+        Next
+        Console.WriteLine()
+
+        Console.Read()
+    End Sub
+
+End Module
